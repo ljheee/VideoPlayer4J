ea5b1dc423056138801abac6d2e090c39ddbbd30
ljh_acer <554278334@qq.com>
2017-06-05 13:40:02 +0800

    修复乱码，工程encoding为UTF-8。

--- src/com/ljheee/video/matAction/LinearAlgebraBasic.java
+++ src/com/ljheee/video/matAction/LinearAlgebraBasic.java
@@ -9,22 +9,22 @@ public class LinearAlgebraBasic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Mat m1=new Mat(2,2,CvType.CV_32FC1);//2*2�ľ���m1��Ԫ��������1ͨ�������ͣ���Ϊ32λ��������float����
-		m1.put(0, 0, new float[]{1,2,3,4});//Ϊm1����ֵ
-		System.out.println("m1����Ϊ��"+m1.dump());
-		System.out.println("m1��ת�þ���Ϊ��"+m1.t().dump());//m1.t()Ϊm1��ת�þ���
-		System.out.println("m1�������Ϊ��"+m1.inv().dump());//m1.inv()Ϊm1�������
+		Mat m1=new Mat(2,2,CvType.CV_32FC1);//2*2的矩阵m1，元素类型是1通道浮点型，因为32位，所以是float类型
+		m1.put(0, 0, new float[]{1,2,3,4});//为m1矩阵赋值
+		System.out.println("m1矩阵为："+m1.dump());
+		System.out.println("m1的转置矩阵为："+m1.t().dump());//m1.t()为m1的转置矩阵
+		System.out.println("m1的逆矩阵为："+m1.inv().dump());//m1.inv()为m1的逆矩阵
 		Mat m2=new Mat();
-		m2.push_back(m1);//��m1���������ֵ�ŵ�m2����ĺ���
+		m2.push_back(m1);//将m1矩阵的所有值放到m2矩阵的后面
 		m2.push_back(m1);
-		System.out.println("m2�ǽ�m1����������εĽ����"+m2.dump());
+		System.out.println("m2是将m1放置其后两次的结果："+m2.dump());
 		
-		Mat m3=Mat.eye(2, 2, CvType.CV_8UC1);//����2*2�ĵ�λ����
-		Mat m4=Mat.ones(2, 2, CvType.CV_8UC1);//����2*2��ȫ1����
-		Mat m5=Mat.zeros(2, 2, CvType.CV_8UC1);//����2*2��ȫ0����
-		System.out.println("�����ĵ�λ����"+m3.dump());
-		System.out.println("������ȫ1����"+m4.dump());
-		System.out.println("������ȫ0����"+m5.dump());
+		Mat m3=Mat.eye(2, 2, CvType.CV_8UC1);//创建2*2的单位矩阵
+		Mat m4=Mat.ones(2, 2, CvType.CV_8UC1);//创建2*2的全1矩阵
+		Mat m5=Mat.zeros(2, 2, CvType.CV_8UC1);//创建2*2的全0矩阵
+		System.out.println("创建的单位矩阵："+m3.dump());
+		System.out.println("创建的全1矩阵："+m4.dump());
+		System.out.println("创建的全0矩阵："+m5.dump());
 	}
 
 }
